dc9084087958d24b02131398a94741fea12253c9
erikaosgue <1476@holbertonschool.com>
2020-05-12 01:54:13 +0000

    Upload printf project

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+int main(void)
+{
+	_printf("%S\n", "Holberton\nSchool");
+	_printf("%S\n", "No special character.");
+	_printf("%S\n", "\n");
+	_printf("%S\n", "\x01\x02\x03\x04\x05\x06\x07");
+	_printf("");
+	_printf("- What did you say?\n- %S\n- That's what I thought.\n", "");
+	return (0);
+}
